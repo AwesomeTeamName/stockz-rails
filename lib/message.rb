7d2004806b237006d396b9097d6559b5be81e6df
Jack Wilsdon <jack.wilsdon2@gmail.com>
2014-08-01 21:53:42 +0100

    Add sender attr

--- lib/message.rb
+++ lib/message.rb
@@ -2,17 +2,20 @@ class Message
 
   # Create public readers for values
   attr_reader :raw
+  attr_reader :sender
   attr_reader :action
   attr_reader :arguments
 
-  def initialize(raw, action, arguments = [])
+  def initialize(raw, sender, action, arguments = [])
     # Validate argument types
     raise TypeError, "wrong argument type #{raw.class.name} for raw (expected String)" unless raw.is_a?(String)
+    raise TypeError, "wrong argument type #{sender.class.name} for sender (expected String)" unless sender.is_a?(String)
     raise TypeError, "wrong argument type #{action.class.name} for action (expected String)" unless action.is_a?(String)
     raise TypeError, "wrong argument type #{arguments.class.name} for arguments (expected Array)" unless arguments.is_a?(Array)
 
     # Assign values
     @raw ||= raw
+    @sender ||= sender
     @action ||= action
     @arguments ||= arguments
   end
@@ -20,6 +23,7 @@ class Message
   # Check whether the current Message is valid
   def is_valid?
     return false unless @raw.is_a?(String) and @raw.length > 0
+    return false unless @sender.is_a?(String) and @sender.length > 0
     return false unless @action.is_a?(String) and @action.length > 0
     return false unless @arguments.is_a?(Array)
 
@@ -30,6 +34,7 @@ class Message
   private
 
   attr_writer :raw
+  attr_writer :sender
   attr_writer :action
   attr_writer :arguments
 end
