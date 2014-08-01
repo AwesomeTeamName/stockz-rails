14fa94c7cde9fb4d3ce63bf347f7aadea75dc054
Jack Wilsdon <jack.wilsdon2@gmail.com>
2014-08-01 08:50:12 +0100

    Document Twii concern and fix formatting

--- app/controllers/concerns/twii.rb
+++ app/controllers/concerns/twii.rb
@@ -3,23 +3,32 @@ require 'twilio-ruby'
 module Twii
   extend ActiveSupport::Concern
 
+  # Set the correct headers for a TwiML document
+  # To be used with after_filter
   def set_header
     response.headers['Content-Type'] = 'text/xml'
   end
 
+  # Render a TwiML::Response
   def render_twiml(response)
-    raise TypeError, "wrong argument type #{response.class.name} for response (expected Twilio::TwiML::Response)" unless response.is_a? Twilio::TwiML::Response
+    # Validate argument type
+    raise TypeError, "wrong argument type #{response.class.name} for response (expected Twilio::TwiML::Response)" unless response.is_a?(Twilio::TwiML::Response)
 
+    # Render the response's text
     render text: response.text
   end
 
+  # Render a plaintext message inside <Message> tags.
   def render_twiml_message(message)
-    raise TypeError, "wrong argument type #{message.class.name} for message (expected String)" unless message.is_a? String
+    # Validate argument type
+    raise TypeError, "wrong argument type #{message.class.name} for message (expected String)" unless message.is_a?(String)
 
+    # Generate the response
     response = Twilio::TwiML::Response.new do |r|
       r.Message(message)
     end
 
+    # Call the render method for responses
     render_twiml(response)
   end
 end
