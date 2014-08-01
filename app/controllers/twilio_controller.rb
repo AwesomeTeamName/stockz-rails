7c1c640164c7a55badc8aa66f47aab7d78ab376b
Jack Wilsdon <jack.wilsdon2@gmail.com>
2014-08-01 02:14:50 +0100

    Set the correct headers and bypass CSRF checking

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,6 +1,9 @@
 class TwilioController < ApplicationController
   include Twii
 
+  after_filter :set_header
+  skip_before_filter :verify_authenticity_token
+
   def sms
     if !params.has_key?('From') || !params.has_key?('Body')
       render status: 400
