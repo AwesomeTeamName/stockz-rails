a406b1c4f978d0b444a151e6505bc19914dc850b
Jack Wilsdon <jack.wilsdon2@gmail.com>
2014-08-01 21:55:35 +0100

    Add comments and integrate parser

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,19 +1,27 @@
 class TwilioController < ApplicationController
   include Twii
 
+  # Set the correct headers
   after_filter :set_header
+
+  # Disable CSRF protection
   skip_before_filter :verify_authenticity_token
 
+  # Twilio SMS integration
   def sms
+    # Ensure a valid request has been sent
     if !params.has_key?('From') || !params.has_key?('Body')
-      render status: 400
+      render text: "400"
       return
     end
 
+    # Get request information
     from = params[:From]
     body = params[:Body]
 
     logger.info("Received message from #{from}: #{body}")
+
+    message = Parser.parse(body)
 
     render_twiml_message('Hello, client!')
   end
