e03d06aec213dcd3d9b27050553c803c164b6419
Niam Patel <naz1997@googlemail.com>
2014-08-02 09:13:52 +0100

    Add stock route

---
+++ app/controllers/stock_controller.rb
@@ -0,0 +1,13 @@
+class StockController < ApplicationController
+  def index
+
+  end
+
+  def view
+
+  end
+
+  def data
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'stock/index'
+
+  get 'stock/view'
+
+  get 'stock/data'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
